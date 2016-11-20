5dba1eb71f9f4e5f274dbf3212204217d56f7942
kazuhikoh <kazuhiko.hrmt@gmail.com>
2016-11-20 22:51:20 +0900

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname ${BASH_SOURCE})
-DOTFILES='dotfiles'
-DIRCOLORS='dircolors'
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-if [ -d $DOTFILES ]; then
-  echo "${DOTFILES} not found!"
-  exit 0
-fi
-
-echo 'Deploy dotfiles ...'
+cd $SCRIPT_DIR
 
 for f in .??*
 do
   [[ $f = ".git" ]] && continue
   [[ $f = ".gitignore" ]] && continue
-  ln -snfv $DOTFILES/$f $HOME/$f
+  ln -svf $SCRIPT_DIR/$f $HOME/$f
 done
 
-ln -s $DOTFILES/$DIRCOLORS/dircolors.ansi-light $HOME/.dir_colors
+ln -svf $SCRIPT_DIR/dircolors/dircolors.ansi-light $HOME/.dir_colors
 
-echo 'Complete!!'
