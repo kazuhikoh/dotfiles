a88b6138ffe2cdf530996e92143944347c372102
kazuhikoh <kazuhiko.hrmt@gmail.com>
2018-05-03 14:00:24 +0900

    agqr録画テスト: エラー時のみSlack通知

--- bin/cron/record_agqr_test.sh
+++ bin/cron/record_agqr_test.sh
@@ -4,4 +4,4 @@ PATH="$PATH:~/bin"
 
 readonly OUTPUT="/tmp/agqr.$(date '+%y%m%d.%H%M%S').flv"
 
-agrec 5 "${OUTPUT}" | slk -w "$1" -c "$2"
+agrec 5 "${OUTPUT}" 3>&1 1>/dev/null 2>&3 | slk -w "$1" -c "$2"
