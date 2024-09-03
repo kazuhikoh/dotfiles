00046e656e30ea23d52c360038395438c166d8d2
kazuhikoh <kazuhiko.hrmt@gmail.com>
2024-09-04 02:31:46 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,44 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
+readonly BACKUP_TIMESTAMP="$(date '+%Y%m%d%H%M%S')"
+
 # Link dotfiles
 for f in .??*
 do
   [[ $f = ".git" ]] && continue
   [[ $f = ".gitignore" ]] && continue
   [[ $f = ".config" ]] && continue
+  [[ $f = ".vim" ]] && continue
 
   ln -svf $SCRIPT_DIR/$f $HOME/$f
 done
 
 # Link .config/*
 [[ ! -e $HOME/.config ]] && mkdir $HOME/.config
-for dir in .config/*
+for it in .config/*
 do
-  ln -svf $SCRIPT_DIR/$dir $HOME/$dir
+  [[ -L $HOME/$it ]] && continue
+  [[ -e $HOME/$it ]] && {
+    bk="$HOME/${it}_${BACKUP_TIMESTAMP}"
+    echo "backup: $HOME/$it -> $bk" 
+    mv $HOME/$it $bk
+  }
+  ln -svf $SCRIPT_DIR/$it $HOME/$it
+done
+
+# Link .vim/*
+[[ ! -e $HOME/.vim ]] && mkdir $HOME/.vim
+for it in .vim/*
+do
+  [[ -L $HOME/$it ]] && continue
+  [[ -e $HOME/$it ]] && {
+    bk="$HOME/${it}_${BACKUP_TIMESTAMP}"
+    echo "backup: $HOME/$it -> $bk"
+    mv $HOME/$it $bk  
+  }
+  ln -svf $SCRIPT_DIR/$it $HOME/$it
+  # => $HOME/.vim/vimrc --> $SCRIPT_DIR/.vim/vimrc
 done
 
 # Link bin/
